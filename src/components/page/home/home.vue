2834011a814b040ff1f7f3338b529b0c010d8479
sjmds <542321969@qq.com>
2017-08-28 20:34:04 +0800

    修改路由

--- src/components/page/home/home.vue
+++ src/components/page/home/home.vue
@@ -6,30 +6,33 @@
       <p>欢迎来到优产到家后台管理系统~</p>
     </div>
     <div class="nav-home clearfix">
-      <router-link to="goodsList" tag="dl">
+      <dl>
+        <router-link to="goodsList">
           <dt><img src="static/images/ico-goods.png"></dt>
           <dd>
             <h3>商品管理</h3>
             <p>快速查阅商品信息</p>
           </dd>
-        </a>
-      </router-link>
-      <router-link to="userList" tag="dl">
+        </router-link>
+      </dl>
+      <dl>
+        <router-link to="userList">
           <dt><img src="static/images/ico-user.png"></dt>
           <dd>
             <h3>新增用户</h3>
             <p>及时了解用户信息</p>
-          </dd>
-        </a>
-      </router-link>
-      <router-link to="store" tag="dl">
+          </dd>        
+        </router-link>
+      </dl>
+      <dl>
+        <router-link to="store">
           <dt><img src="static/images/ico-inventory.png"></dt>
           <dd>
             <h3>库存查询</h3>
             <p>库存信息，一目了然</p>
-          </dd>
-        </a>
-      </router-link>
+          </dd>        
+        </router-link>
+      </dl>
     </div>
     <!-- say-hello -->
   </div>
